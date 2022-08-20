e6c485ddc2d4c257da3c51ce8b128dad0c8c140b
mandeepharsh <100046979+mandeepharsh@users.noreply.github.com>
2022-08-20 15:10:39 -0700

    Updates

---
+++ precache-manifest.6ce6bf6e594fc5e16fe41826a0800de1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "65a0b02dfba2566936c0",
+    "url": "/robot/static/css/main.e4a4da30.chunk.css"
+  },
+  {
+    "revision": "65a0b02dfba2566936c0",
+    "url": "/robot/static/js/main.412e8535.chunk.js"
+  },
+  {
+    "revision": "f06414c6efce8d746529",
+    "url": "/robot/static/js/runtime~main.1a45230c.js"
+  },
+  {
+    "revision": "4005abe7b88519334713",
+    "url": "/robot/static/css/2.5284535f.chunk.css"
+  },
+  {
+    "revision": "4005abe7b88519334713",
+    "url": "/robot/static/js/2.74959997.chunk.js"
+  },
+  {
+    "revision": "440f8641b4ac1935df7a",
+    "url": "/robot/static/js/3.76c3d8c6.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robot/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c019e490a7357be08f69c4084a9a14a4",
+    "url": "/robot/index.html"
+  }
+];
